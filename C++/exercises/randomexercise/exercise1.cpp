9abede916f51132957371768148d50646fccbc6a
Phil <99208416+megu0iq@users.noreply.github.com>
2023-03-13 10:42:30 -0500

    pepper

---
+++ C++/exercises/randomexercise/exercise1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int add(int x, int y)
+{
+	return x + y;
+}
+
+void printResult(int z)
+{
+	std::cout << "The answer is: " << z << '\n';
+}
+
+int getUserInput()
+{
+	std::cout << "Enter a number: ";
+	int x{};
+	std::cin >> x;
+	return x;
+}
+
+int main()
+{
+	int x{ getUserInput() };
+	int y{ getUserInput() };
+
+	std::cout << x << " + " << y << '\n';
+
+	int z{ add(x, 5) };
+	printResult(z);
+
+	return 0;
+}
