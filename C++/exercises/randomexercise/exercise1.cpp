a618b95edcfe06266dc4e5e80b3220567114257c
Phil <99208416+megu0iq@users.noreply.github.com>
2023-03-13 14:36:59 -0500

    C++

--- C++/exercises/randomexercise/exercise1.cpp
+++ C++/exercises/randomexercise/exercise1.cpp
@@ -12,21 +12,29 @@ void printResult(int z)
 
 int getUserInput()
 {
-	std::cout << "Enter any number: ";
+	std::cout << "Enter any whole number integer: ";
 	int x{};
 	std::cin >> x;
 	return x;
 }
-
+int getUserChoice(int userChoice)
+{
+	std::cout << "Enter 1 to start the program, enter 0 to exit the program." << std::endl;
+	std::cin >> userChoice;
+	return userChoice;
+}
 int main()
 {
+	int userChoice;
+	while (getUserChoice(userChoice) != 0)
+	{
 	int x{ getUserInput() };
 	int y{ getUserInput() };
-
 	std::cout << x << " + " << y << '\n';
 
 	int z{ add(x, 5) };
 	printResult(z);
-
+	int userChoice;
+	}
 	return 0;
 }
