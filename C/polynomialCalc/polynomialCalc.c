27097513a5f309daf45acaf28b022e042a08b75a
megu0iq <meguirep@gmail.com>
2023-03-17 10:02:08 -0500

    C

--- C/polynomialCalc/polynomialCalc.c
+++ C/polynomialCalc/polynomialCalc.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
+#include <math.h>
+
+int exitUser(int userChoice)
+{
+	printf("Continue?, press 0 to exit\n");
+        scanf("%i", &userChoice);//test
+	return userChoice;
+}
 
 int main(void)
 {
     int numTerms, userChoice;
     do
     {
-        printf("How many terms are there in the polynomial?");
+        printf("How many terms are there in the polynomial? (max of 5 terms)");
         scanf("%i", &numTerms);
         if(numTerms >= 5){
             printf("Too many terms for accurate calculation.\n");
+	    exitUser(userChoice);
         }
-        printf("Continue?, press 0 to exit\n");
-        scanf("%i", &userChoice);//test
     } while(userChoice != 0);
 }
